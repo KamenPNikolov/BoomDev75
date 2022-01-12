c65934dfcc47de50b9e4a7b2aa850af83830b103
knikolov <kami_985@abv.bg>
2022-01-12 23:34:37 +0200

    Buttons and event handlers

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import "./App.css";
+import {useState} from "react";
 
 function App() {
+  
+  const [text,setText]= useState(localStorage.getItem("priorValue"));
+
   return (
     <div className="App">
       <div className="box">
         <div className="field">
           <div className="control">
-            <textarea className="textarea is-large" placeholder="Notes..." />
+            <textarea className="textarea is-large" placeholder="Notes..." value = {text} onChange={(event)=>{
+                              setText((text)=>{
+                                console.log(event.target.value);
+                                return event.target.value; 
+                              });
+                            }}/>
           </div>
         </div>
-        <button className="button is-large is-primary is-active">Save</button>
-        <button className="button is-large">Clear</button>
+        <button className="button is-large is-primary is-active" onClick = {()=>{localStorage.setItem("priorValue",text);}}>Save</button>
+        <button className="button is-large" onClick = {()=>{localStorage.setItem("priorValue","");}}>Clear</button>
       </div>
     </div>
   );
